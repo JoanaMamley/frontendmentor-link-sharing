5dd1a35e52226c9bed4543c6c1ebd3a2cbc4d593
JoanaMamley <joanateye18@gmail.com>
2024-08-19 17:07:57 +0000

    add links content on main page

--- src/app/home/links/links.component.ts
+++ src/app/home/links/links.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-links',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './links.component.html',
   styleUrl: './links.component.scss'
 })
