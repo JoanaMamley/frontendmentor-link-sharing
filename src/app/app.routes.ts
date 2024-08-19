b511c7bf50b872ff36553662dccc07abfc7f3a44
JoanaMamley <joanateye18@gmail.com>
2024-08-19 09:52:39 +0000

    add new routes and their components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { LinksComponent } from './home/links/links.component';
+import { ProfileEditComponent } from './home/profile-edit/profile-edit.component';
+import { PreviewComponent } from './preview/preview.component';
 
 export const routes: Routes = [
       {
@@ -10,6 +14,24 @@ export const routes: Routes = [
       {
         path: 'login',
         component: LoginComponent,
+      },
+      {
+        path: 'home',
+        component: HomeComponent, 
+        children: [
+          {
+            path: 'links', 
+            component: LinksComponent,
+          },
+          {
+            path: 'profile',
+            component: ProfileEditComponent, 
+          },
+        ],
+      },
+      {
+        path: 'preview',
+        component: PreviewComponent,
       },
       { path: '',   redirectTo: '/register', pathMatch: 'full' }, 
 ];
