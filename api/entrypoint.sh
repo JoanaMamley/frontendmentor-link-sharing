d94111f39c8dab7f88e15abb499bea9e583b1944
JoanaMamley <joanateye18@gmail.com>
2025-04-10 15:23:16 +0000

    ensure app reloads on file changes

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-# Wait for MySQL to be ready
 echo "Waiting for MySQL..."
 
 while ! nc -z db 3306; do
   sleep 1
 done
 
-echo "MySQL is up. Starting Flask app..."
+echo "MySQL is up. Starting Flask..."
 
-flask run --host=0.0.0.0 --port=5000
+flask run --host=0.0.0.0 --port=5000 --reload
