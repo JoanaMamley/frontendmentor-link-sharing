a76eb936fccd9d613a4dc8b755aaf30c48a2cea9
JoanaMamley <joanateye18@gmail.com>
2025-04-11 16:06:57 +0000

    send login details to auth service when user clicks login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../shared/services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -10,27 +12,48 @@ import { Router, RouterLink } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit, OnDestroy {
   loginForm?: FormGroup;
+  loginError: string | null = null;
+  subscriptions: Subscription[] = [];
 
-  constructor(private router: Router){}
+  constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit(): void {
     this.loginForm = new FormGroup({
       'email': new FormControl<null | string>(null, [Validators.required, Validators.email]),
       'password': new FormControl<null | string>(null, [Validators.required, Validators.minLength(8)]),
-    })
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 
   onSubmit() {
-    this.router.navigateByUrl('/home');
-   }
- 
-   get email(): AbstractControl<any, any> | null | undefined {
-     return this.loginForm?.get('email');
-   }
- 
-   get password(): AbstractControl<any, any> | null | undefined {
-     return this.loginForm?.get('password');
-   }
+    if (this.loginForm?.valid) {
+      const sub: Subscription = this.authService.login({
+        email: this.loginForm?.value.email,
+        password: this.loginForm?.value.password
+      }).subscribe({
+        next: () => {
+          this.loginError = null;
+          this.router.navigateByUrl('/home');
+        },
+        error: (err) => {
+          this.loginError = err.error.message;
+        }
+      });
+
+      this.subscriptions.push(sub);
+    }
+  }
+
+  get email(): AbstractControl<any, any> | null | undefined {
+    return this.loginForm?.get('email');
+  }
+
+  get password(): AbstractControl<any, any> | null | undefined {
+    return this.loginForm?.get('password');
+  }
 }
