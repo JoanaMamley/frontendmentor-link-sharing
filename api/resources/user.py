1b1475628a8939752cc0fa14b269dc47344b07ec
JoanaMamley <joanateye18@gmail.com>
2025-04-12 07:06:32 +0000

    add used import

--- api/resources/user.py
+++ api/resources/user.py
@@ -1,3 +1,4 @@
+from flask import jsonify
 from flask.views import MethodView
 from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt, get_jwt_identity, jwt_required
 from flask_smorest import Blueprint, abort
