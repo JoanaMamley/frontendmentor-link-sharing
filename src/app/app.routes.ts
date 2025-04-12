e1e17a5e4b18604e6c1a249dc8aa18b95e4312e2
JoanaMamley <joanateye18@gmail.com>
2025-04-12 18:06:18 +0000

    add auth guard to ensure home page is only accessible to authenticated users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home/home.component';
 import { LinksComponent } from './home/links/links.component';
 import { ProfileEditComponent } from './home/profile-edit/profile-edit.component';
 import { PreviewComponent } from './preview/preview.component';
+import { AuthGuard } from './shared/guards/auth.guard';
 
 export const routes: Routes = [
       {
@@ -29,6 +30,7 @@ export const routes: Routes = [
             component: ProfileEditComponent, 
           },
         ],
+        canActivate: [AuthGuard],
       },
       {
         path: 'preview',
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,24 +1,36 @@
 // auth.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { lastValueFrom, Observable } from 'rxjs';
 import { environment } from '../environments/environment';
+import { AuthRequest, GenericResponse } from '../models/generic.model';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private _isLoggedIn: boolean = false;
+
   constructor(private http: HttpClient) {}
 
-  login(credentials: any): Observable<any> {
-    return this.http.post<any>(`${environment.apiUrl}/login`, credentials, { withCredentials: true });
+  login(credentials: AuthRequest): Promise<GenericResponse> {
+    return lastValueFrom(this.http.post<GenericResponse>(`${environment.apiUrl}/login`, credentials, { withCredentials: true }));
   }
 
-  logout(): Observable<any> {
-    return this.http.post(`${environment.apiUrl}/logout`, {}, { withCredentials: true });
+  logout(): Observable<GenericResponse> {
+    return this.http.post<GenericResponse>(`${environment.apiUrl}/logout`, {}, { withCredentials: true });
   }
 
-  refreshAccessToken(): Observable<any> {
-    return this.http.post<any>(`${environment.apiUrl}/refresh`, {}, { withCredentials: true });
+  refreshAccessToken(): Observable<GenericResponse> {
+    return this.http.post<GenericResponse>(`${environment.apiUrl}/refresh`, {}, { withCredentials: true });
+  }
+
+  set isLoggedIn(value: boolean) {
+    this._isLoggedIn = value;
+  }
+
+  get isLoggedIn(): boolean {
+    return this._isLoggedIn;
   }
 }
