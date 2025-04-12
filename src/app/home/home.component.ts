a18a7d274e7e04e00f22b89e55307ace536dbe2c
JoanaMamley <joanateye18@gmail.com>
2025-04-12 07:01:43 +0000

    fetch current user in frontend

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
 import { PhoneMockupComponent } from './phone-mockup/phone-mockup.component';
+import { UserService } from '../shared/services/user.service';
+import { Subscription } from 'rxjs';
+import { User } from '../shared/models/user.model';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +13,26 @@ import { PhoneMockupComponent } from './phone-mockup/phone-mockup.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  subscriptions: Subscription[] = [];
+  user?: User;
 
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    const sub: Subscription = this.userService.getCurrentUser().subscribe({
+      next: (user) => {
+        this.user = user;
+      },
+      error: (error) => {
+        console.error('Error fetching current user:', error);
+      }
+    });
+
+    this.subscriptions.push(sub);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
 }
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment';
+import { User } from '../models/user.model';
 
 interface RegisterResponse {
   message: string;
@@ -21,5 +22,9 @@ export class UserService {
 
   register(userData: RegisterRequest): Observable<RegisterResponse> {
     return this.http.post<RegisterResponse>(`${environment.apiUrl}/register`, userData);
+  }
+
+  getCurrentUser(): Observable<User> {
+    return this.http.get<User>(`${environment.apiUrl}/user/me`, { withCredentials: true });
   }
 }
