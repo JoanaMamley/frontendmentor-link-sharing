5197256db0e17814430542ad026bfe0071bac235
JoanaMamley <joanateye18@gmail.com>
2025-04-22 21:57:30 +0000

    add link service and method for adding link

---
+++ src/app/shared/services/link.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Link, RawLinkItem } from '../models/link.model';
+import { UserService } from './user.service';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LinkService {
+  private linksSubject = new BehaviorSubject<Link[]>([]);
+  links$: Observable<Link[]> = this.linksSubject.asObservable();
+
+  constructor(private userService: UserService, private http: HttpClient) {
+    this.loadSavedLinks();
+  }
+
+  private loadSavedLinks() {
+    this.userService.getCurrentUser().subscribe(user => {
+      const savedLinks = user.links.map(link => ({ ...link, isEditing: false }));
+      this.linksSubject.next(savedLinks);
+    });
+  }
+
+  addNewLink() {
+    const currentLinks = this.linksSubject.getValue();
+    const updatedLinks = currentLinks.map(link => ({ ...link, isEditing: false }));
+    updatedLinks.push({ id: -1, link_type: '', link_url: '', isEditing: true });
+    this.linksSubject.next(updatedLinks);
+  }
+
+  private addLink(link: Link): Observable<RawLinkItem> {
+    return this.http.post<RawLinkItem>(`${environment.apiUrl}/link`, link, { withCredentials: true });
+  }
+
+  saveLink(newLink: Link) {
+    this.addLink(newLink).subscribe(updatedLink => {
+      const currentLinks = this.linksSubject.getValue();
+      const updatedLinks = currentLinks.map(link => {
+        if (link.id === -1) {
+          return { ...link, id: updatedLink.id, link_type: updatedLink.link_type, link_url: updatedLink.link_url, isEditing: false };
+        }
+        return link;
+      });
+      this.linksSubject.next(updatedLinks);
+    });
+  }
+}
