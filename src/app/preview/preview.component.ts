b8a7b912f88f41ec17384c06b08e3dffdbcb8064
JoanaMamley <joanateye18@gmail.com>
2025-04-29 19:41:16 +0000

    enable navigation to editor from preview page

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { LINKDETAILS } from '../shared/models/link.model';
+import { Router } from '@angular/router';
 
 interface LinkDesc{
   name: string;
@@ -17,7 +18,13 @@ interface LinkDesc{
 export class PreviewComponent {
   linkItems: LinkDesc[] = [LINKDETAILS['github'], LINKDETAILS['youtube'], LINKDETAILS['linkedIn']];
 
+  constructor(private router: Router){}
+
   getlinkIcon(linkType: LinkDesc): string {
     return `images/${linkType.icon}`;
+  }
+
+  navigateToEditor() {
+    this.router.navigateByUrl('/home');
   }
 }
